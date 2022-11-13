a3514ab74c6fa04b672c90eaa8ffe6060ee63bc0
gentoumashiro <gentoumashiro@gmail.com>
2022-11-13 16:23:41 +0300

    now search files faster

--- src/get_paths.py
+++ src/get_paths.py
@@ -1,12 +1,13 @@
 import os
 import threading
+import time
 
 from psutil import disk_partitions
-from .GetTime_func import total_time
-
-disks = tuple(i.device for i in disk_partitions())
+from src.colors import Colors
+disks = tuple(i.device for i in disk_partitions())[::-1]
 
 genshin_path = False
+c = Colors()
 
 
 def searching_full_genshinpath(disk, filename="GenshinImpact.exe") -> None:
@@ -16,13 +17,22 @@ def searching_full_genshinpath(disk, filename="GenshinImpact.exe") -> None:
     global genshin_path
 
     for root, _, files in os.walk(disk):
+        print(root)
+        if genshin_path:
+            break
         if filename in files:
+            print(f"{c.green}{filename} was founded in {root}!{c.RESET}")
             genshin_path = os.path.abspath(f"{root}\\{filename}")
+            break
 
 
-@total_time
 def multithreading_search(func=searching_full_genshinpath) -> tuple | bool:
+    print(f"Starting GenshinImpact.exe researcher func\nIt may take some time...")
+    time.sleep(1)
+
     threads = []
+
+    global genshin_path
 
     for disk in disks:
         t = threading.Thread(target=func, args=(disk,))
